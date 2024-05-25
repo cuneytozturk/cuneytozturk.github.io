4ae9acc1ded4c5feba102c29ec571662ffc41eb9
cuneytozturk <cuneytozturk17@gmail.com>
2024-05-25 17:03:05 +0200

    first version of website.

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".hamburger").addEventListener("click", function () {
+    this.classList.toggle("open");
+  });
+});
